386f7361fa2a366cc81158e946c748b9a6c688a3
Steven Hsiao <stevenhsiao@starlux-airlines.com>
2018-04-16 16:30:35 +0800

    attract header to Header.vue

--- src/views/JobBoardIndex.vue
+++ src/views/JobBoardIndex.vue
@@ -1,29 +1,9 @@
 <template>
     <div class="layout">
         <Layout>
-            <Header :style="{position: 'fixed', width: '100%'}">
-                <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
-                    <div class="layout-nav">
-                        <MenuItem name="1">
-                            <Icon type="ios-navigate"></Icon>
-                            Item 1
-                        </MenuItem>
-                        <MenuItem name="2">
-                            <Icon type="ios-keypad"></Icon>
-                            Item 2
-                        </MenuItem>
-                        <MenuItem name="3">
-                            <Icon type="ios-analytics"></Icon>
-                            Item 3
-                        </MenuItem>
-                        <MenuItem name="4">
-                            <Icon type="ios-paper"></Icon>
-                            Item 4
-                        </MenuItem>
-                    </div>
-                </Menu>
-            </Header>
+            <nav-header>
+            </nav-header>
+
             <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
                 Content
             </Content>
@@ -33,7 +13,15 @@
 </template>
 
 <script>
+    import NavHeader from '../components/Header.vue'
+
+
+
+
     export default {
+        components:{
+            NavHeader,  // 沒有用key valu 會自動生成key和value名稱相同
+        }
         
     }
 </script>
@@ -45,21 +33,6 @@
         position: relative;
         border-radius: 4px;
         overflow: hidden;
-    }
-    .layout-logo{
-        width: 100px;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        float: left;
-        position: relative;
-        top: 15px;
-        left: 20px;
-    }
-    .layout-nav{
-        width: 420px;
-        margin: 0 auto;
-        margin-right: 20px;
     }
     .layout-footer-center{
         text-align: center;
